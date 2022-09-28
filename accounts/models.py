fe6cff3ea98986586a82b412b8577a38a1e31d9b
Henry Golding <hgolding@protonmail.com>
2022-09-28 13:55:00 +0100

    feat: created customer model

--- accounts/models.py
+++ accounts/models.py
@@ -46,3 +46,14 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class Customer(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    address_line1 = models.CharField(max_length=200)
+    address_line2 = models.CharField(max_length=200, blank=True)
+    address_line3 = models.CharField(max_length=200, blank=True)
+    address_line4 = models.CharField(max_length=200, blank=True)
+    postcode = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
