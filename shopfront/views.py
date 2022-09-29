19b2acce8bbefef9f6c26f5be6f07029494453a5
Henry Golding <hgolding@protonmail.com>
2022-09-29 12:54:53 +0100

    feat: basic search functionality

--- shopfront/views.py
+++ shopfront/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import get_object_or_404, render
 from .models import Wine
 
 def index(request):
+    countries = [wine.country for wine in Wine.objects.distinct('country')]
+    colors = [wine.color for wine in Wine.objects.distinct('color')]
+    if request.method == 'POST':
+        request.session['last_search'] = request.POST
+        print(request.session['last_search']) 
+        return search(request)
     context = {
-        'countries': [wine.country for wine in Wine.objects.distinct('country')],
-        'colors': [wine.color for wine in Wine.objects.distinct('color')],
-    }
+        'countries': countries,
+        'colors': colors,
+        }
     return render(request, 'shopfront/index.html', context)
 
 def product_details(request, pk):
@@ -14,3 +20,19 @@ def product_details(request, pk):
         'wine': get_object_or_404(Wine, id=pk),
     }
     return render(request, 'shopfront/product.html', context)
+
+def search(request):
+    countries = [wine.country for wine in Wine.objects.distinct('country')]
+    colors = [wine.color for wine in Wine.objects.distinct('color')]
+    search = request.session['last_search']
+    wines = Wine.objects.filter(price_incvat__range=(search['price'].split()[0], search['price'].split()[1]))
+    if search['country'] in countries:
+        wines = wines.filter(country=search['country'])
+    if search['color'] in colors:
+        wines = wines.filter(color=search['color'])
+    print(wines) 
+    context = {
+        'wines': wines,
+        'sort': search['sort'],
+    }
+    return render(request, 'shopfront/winelist.html', context)
