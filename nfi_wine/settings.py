347b7bf34e26f60aaf2530f287e016502f3bbb39
Henry Golding <hgolding@protonmail.com>
2022-10-03 14:08:51 +0100

    fix: change settings to accept api calls from vue

--- nfi_wine/settings.py
+++ nfi_wine/settings.py
@@ -48,9 +48,15 @@ INSTALLED_APPS = [
     'checkout.apps.CheckoutConfig',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
